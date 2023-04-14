15c1501c39356bfbffdd382013ef6337f11976d8
Shakthi-2406 <shakthisri504@gmail.com>
2023-04-14 23:43:26 +0530

    dddd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+      state = {
+          isLoading: true,
+          users: [],
+          error: null
+      };
 
+      getFetchUsers() {
+          this.setState({
+              isloading: true
+          }, () => {
+              fetch("http://localhost:8080/api/tutorials").then(res => res.json()).then(result => this.setState({
+                  isloading: false,
+                  users: result
+              })).catch(console.log);
+          });
+      }
+
+      componentDidMount() {
+        this.getFetchUsers();
+      }
+
+      render() {
+          const {users,error} = this.state;
+          return (
+          <div className="App">
+            <h1>All Users</h1>
+          {users.map((data) => {
+              return(
+                <div>
+                  {data.id + data.name + data.user_place + data.user_email}
+                </div>);
+              }
+            )
+          }
+          </div>  
+          );
+        }
+      }
+      
 export default App;
